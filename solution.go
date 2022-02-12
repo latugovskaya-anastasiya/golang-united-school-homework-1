86bcdfa9368849191902d617c4070b5ec0e9288d
Anastasiya Latugovskaya <latugovskayawork@gmail.com>
2022-02-12 16:32:32 +0300

    Introduction_task

--- solution.go
+++ solution.go
@@ -1,5 +1,8 @@
 package solution
 
+import "github.com/kyokomi/emoji"
+
+// GetMessage returns an invitation string with emoji
 func GetMessage() string {
-	return ""
+	return emoji.Sprint("Hello :world_map:!")
 }
